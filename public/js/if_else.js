ea2354723a60ba7db060df6a485798b4a2d2168e
Richard <Richard@Richards-MacBook-Pro-2.local>
2016-02-04 08:45:56 -0600

    Practicing my JS

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -8,24 +8,28 @@ var favorite = 'blue'; // TODO: change this to your favorite color from the list
 // TODO: Create a block of if/else statements to check for every color except indigo and violet.
 // TODO: When a color is encountered log a message that tells the color, and an object of that color.
 //       Example: Blue is the color of the sky.
-if (color =='red') {
-	console.log('firetruck is red');
+if (color == 'red') {
+		console.log('firetruck is red');
 } else if (color == 'orange') {
-	console.log('oranges are orange');
+		console.log('oranges are orange');
 } else if (color == 'yellow') {
-	console.log('schoolbus is yellow');
+		console.log('Bananas are yellow');
 } else if (color == 'green') {
-	console.log('grass is green');
+		console.log('leaves are green');
 } else if (color == 'blue') {
-	console.log('sky is blue');
-// TODO: Have a final else that will catch indigo and violet.
-// TODO: In the else, log: I do not know anything by that color.
+		console.log('The sky is blue');
 } else {
-	console.log('I do not know anything by that color.');
+		console.log(' I do not know anything by that color ' + color);
 }
 
+(color == favorite) ? console.log ('It caught blue which is my favorite color.') : console.log('no Dice');
 
-var mycolor = (color==favorite) ? favorite + "it caught blue which is my favorite color." : color + "No Dice";
+
+// TODO: Have a final else that will catch indigo and violet.
+// TODO: In the else, log: I do not know anything by that color.
+
+
+
 	
 
 // TODO: Using the ternary operator, conditionally log a statement that
@@ -34,11 +38,4 @@ var mycolor = (color==favorite) ? favorite + "it caught blue which is my favorit
 // if/else example
 
 
-var i = 0;
-
-while (i < 10) {
-	console.log(i);
-	i++;
-}
-console.log('done');
 
